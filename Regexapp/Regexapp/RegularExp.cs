0241e14e596cb7569914ece7c3401bc1cef8f754
addevelop <adrienpro.rodrigues@gmail.com>
2021-11-04 17:22:24 +0100

    Regex 1

--- Regexapp/Regexapp/RegularExp.cs
+++ Regexapp/Regexapp/RegularExp.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Regexapp
 {
     class RegularExp
     {
+        string rgx;
+        string str;
+        
+        public RegularExp(string rgx, string str)
+        {
+            this.rgx = rgx;
+            this.str = str;
+        }
+
+        public bool Mregex()
+        {
+            Match mt = Regex.Match(this.str, this.rgx);
+            if (mt.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+           
+        
+        
+        
+
     }
 }
